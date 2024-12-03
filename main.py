33997c40005220324dc132e874216b7933f46138
DPC <danielcolosa@gmail.com>
2024-12-03 14:42:45 -0500

    formatting alerts

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from nws_api import fetch_nws_alerts
+from nws_api import fetch_nws_alerts, format_nws_alert
 
 
 def main():
  
     alerts_data = fetch_nws_alerts()
+
+    formatted = format_nws_alert(alerts_data)
+
+    print(formatted)
 
 if __name__ == "__main__":
     main()
--- nws_api.py
+++ nws_api.py
@@ -30,7 +30,30 @@ def fetch_nws_alerts():
     response.raise_for_status()  
     return response.json()
 
+def format_nws_alert(input_data):
 
+    try:
+        
+        if isinstance(input_data, str):
+            input_data = input_data.replace("'", "\"")  
+            parsed_data = json.loads(input_data)
+        elif isinstance(input_data, dict):
+            parsed_data = input_data
+        else:
+            raise ValueError("Unsupported input type. Must be a string or dictionary.")
+        
+        
+        if 'sageType' in parsed_data:
+            parsed_data['messageType'] = parsed_data.pop('sageType')
+
+        
+        formatted_data = json.dumps(parsed_data, indent=4)
+        return formatted_data
+
+    except Exception as e:
+        return f"Error formatting data: {e}"
+    
+    
 #TODO
 # Most likey just use matplot lib to generate a percipitation map around the location of the user
 # Ill keep the api key in there incase i need to use a different api which i prob will need to
